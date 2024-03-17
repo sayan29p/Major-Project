8f3721b301d3adbfb1b1ca6708e70d954e840578
sayan29p <sayan29p@gmail.com>
2024-03-17 18:54:55 +0530

    Added Major-Project Files

---
+++ page.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify, Response
+from app import gen
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
